03ca0f2b0884cf6b59c2b2f7f0442b72e5ff67f1
Stas <leshuksu@gmail.com>
2022-12-12 21:56:14 +0300

    Installed Semantic UI React

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { HashRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
