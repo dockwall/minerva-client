e03a6c60235fc7dd9d14679451ff7eb97412fc8c
Stas <leshuksu@gmail.com>
2022-12-12 08:31:28 +0300

    Scaffolded pages components, used HashRouter

---
+++ src/components/streams/StreamList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StreamList = () => {
+    return (
+        <div>
+            <h1>Stream List Page</h1>
+        </div>
+    )
+};
+
+export default StreamList;
