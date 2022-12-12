c7639b13b24f9ef1c2457a408bb8a25c943fccde
Stas <leshuksu@gmail.com>
2022-12-12 14:37:36 +0300

    Wired routes, checked all pathes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import StreamList from "./streams/StreamList";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
     return (
-        <div>Hello!</div>
+        <Routes>
+            <Route path="/" element={<StreamList />} />
+            <Route path="/new" element={<StreamCreate />} />
+            <Route path="/delete" element={<StreamDelete />} />
+            <Route path="/show" element={<StreamShow />} />
+            <Route path="/edit" element={<StreamEdit />} />
+        </Routes>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // There are 3 Router types:
 // 1 - BrowserRouter. Uses string after top-level domain (TLD) as path (123.com/path1)
-// Good for configured and dev servers, but there are a troubles with classic servers
+// Good for configured and dev servers, but there are a troubles with classic deployment
 
 // 2 - HashRouter. Injects "/#/" in URL after TLD and sees string after it as current path (123.com/#/path1)
 // Good for GitHub pages
 
 // 3 - MemoryRouter. Doesn't use URL as path (no update, no read)
-
 
 root.render(
     <HashRouter>
