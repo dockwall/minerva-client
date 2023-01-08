3fe3c901d34159bfc7cade5f97df19d1e88cf7b1
Stas <leshuksu@gmail.com>
2023-01-08 14:05:01 +0300

    Added initialize and sign in logic

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,9 +2,31 @@ import React from "react";
 import { clientKey } from "../api/google";
 
 class GoogleAuth extends React.Component {
+    componentDidMount() {
+        /* global google */
+
+        function handleCallbackResponse(response) {
+            console.log(response);
+        };
+
+        const idConfig = {
+            client_id: clientKey,
+            callback: handleCallbackResponse
+        };
+
+        const BtnConfig = {
+            theme: 'outline',
+            size: 'large'
+        }
+
+        google.accounts.id.initialize(idConfig);
+
+        google.accounts.id.renderButton(document.getElementById('signInBtn'), BtnConfig)
+    }
+
     render() {
         return (
-            <div>Google Auth!</div>
+            <div id='signInBtn'></div>
         );
     }
 }
