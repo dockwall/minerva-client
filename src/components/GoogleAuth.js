c36d316df0faa7f328484206b073ff9a2ed7657e
Stas <leshuksu@gmail.com>
2023-01-08 14:36:02 +0300

    Add state with data about authorized user

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,22 @@
 import React from "react";
+import jwtDecode from "jwt-decode";
 import { clientKey } from "../api/google";
 
 class GoogleAuth extends React.Component {
+    state = { user: {} }
+
+    handleCallbackResponse = (response) => {
+        const currentUser = jwtDecode(response.credential);
+
+        this.setState({ user: currentUser });
+    };
+
     componentDidMount() {
         /* global google */
 
-        function handleCallbackResponse(response) {
-            console.log(response);
-        };
-
         const idConfig = {
             client_id: clientKey,
-            callback: handleCallbackResponse
+            callback: this.handleCallbackResponse
         };
 
         const BtnConfig = {
@@ -20,14 +25,19 @@ class GoogleAuth extends React.Component {
         }
 
         google.accounts.id.initialize(idConfig);
-
         google.accounts.id.renderButton(document.getElementById('signInBtn'), BtnConfig)
     }
 
+
+    // If we don't have user - show sign in
+    // If we don't have user - show sign out
     render() {
         return (
-            <div id='signInBtn'></div>
-        );
+            <div>
+                {this.state.user.email ? '' : <div id='signInBtn'></div>}
+            </div>
+
+        )
     }
 }
 
